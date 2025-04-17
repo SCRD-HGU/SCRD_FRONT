243044d338ed36d2ab0eb84d9a9d80706e6d2f6a
gmlgks <bins506@gmail.com>
2025-04-17 23:41:06 +0900

    로딩 페이지

--- escape-room/src/pages/KakaoLogin.js
+++ escape-room/src/pages/KakaoLogin.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useSetRecoilState } from "recoil";
+import styled, { keyframes } from "styled-components";
+import LogoImg from "../assets/Logo.svg";
 import {
   userTokenState,
   tokenState,
@@ -17,61 +19,82 @@ const KakaoLogin = () => {
   const setRefreshToken = useSetRecoilState(refreshTokenState);
   const setToken = useSetRecoilState(tokenState);
 
-  // api 요청에 필요한 토큰 저장
-
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    console.log("searchParams : " + searchParams);
     const code = searchParams.get("code");
     setCode(code);
 
     if (code) {
       const apiUrl = `${process.env.REACT_APP_BASE_URL}/auth/kakao-login?code=${code}`;
-      console.log("apiUrl " + apiUrl);
 
       axios
         .get(apiUrl, {
           headers: {
             Referer: "http://localhost:3000",
-            // Referer: "https://elegant-puppy-7c8193.netlify.app/",
           },
         })
         .then((response) => {
-          // 헤더에서 accessToken과 refreshToken을 가져와서 상태에 저장
-          const accessToken = response.headers["authorization"].split(" ")[1]; // Authorization 헤더
-          const refreshToken = response.headers["x-refresh-token"]; // X-Refresh-Token 헤더
+          const accessToken = response.headers["authorization"].split(" ")[1];
+          const refreshToken = response.headers["x-refresh-token"];
 
-          // 성공 시 처리
-          console.log(
-            response.data,
-            "accessToken : ",
-            accessToken,
-            ", refreshToken : " + refreshToken
-          );
           setUserToken({ isLoggedIn: true });
-          setToken(accessToken); // accessToken 저장
-          setRefreshToken(refreshToken); // refreshToken 저장
+          setToken(accessToken);
+          setRefreshToken(refreshToken);
 
           localStorage.setItem("accessToken", accessToken);
           localStorage.setItem("refreshToken", refreshToken);
-          
+
           navigate("/main");
         })
         .catch((error) => {
-          // 오류 시 처리
           console.error("Error during Kakao login:", error);
         });
     }
-  }, [
-    location.search,
-    setCode,
-    setUserToken,
-    setToken,
-    setRefreshToken,
-    navigate,
-  ]);
+  }, [location.search, setCode, setUserToken, setToken, setRefreshToken, navigate]);
 
-  return <div>카카오 로그인 처리 중...</div>;
+  return (
+    <Container>
+      <Logo src={LogoImg} alt="App Logo" />
+      <Message>카카오 계정으로 로그인 중입니다...</Message>
+      <Spinner />
+    </Container>
+  );
 };
 
 export default KakaoLogin;
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fffef9;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+  height: 80px;
+  margin-bottom: 20px;
+`;
+
+const Message = styled.div`
+  font-size: 18px;
+  margin-bottom: 16px;
+  color: #333;
+`;
+
+const Spinner = styled.div`
+  border: 4px solid #e0e0e0;
+  border-top: 4px solid #fae100; /* 카카오 스타일 노랑색 */
+  border-radius: 50%;
+  width: 36px;
+  height: 36px;
+  animation: ${spin} 1s linear infinite;
+`;
